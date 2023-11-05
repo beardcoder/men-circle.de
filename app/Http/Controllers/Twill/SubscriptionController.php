b36746e85404405f9d0b85479443940df586f923
Markus Sommer <markussom@gmail.com>
2023-11-05 15:55:51 +0100

    feat: display subscription data

--- app/Http/Controllers/Twill/SubscriptionController.php
+++ app/Http/Controllers/Twill/SubscriptionController.php
@@ -11,41 +11,56 @@ use A17\Twill\Http\Controllers\Admin\ModuleController as BaseModuleController;
 
 class SubscriptionController extends BaseModuleController
 {
-    protected $moduleName = 'subscriptions';
-    /**
-     * This method can be used to enable/disable defaults. See setUpController in the docs for available options.
-     */
-    protected function setUpController(): void
-    {
-        $this->disablePermalink();
-    }
+  protected $moduleName = 'subscriptions';
+  /**
+   * This method can be used to enable/disable defaults. See setUpController in the docs for available options.
+   */
+  protected function setUpController(): void
+  {
+    $this->disablePermalink();
+  }
 
-    /**
-     * See the table builder docs for more information. If you remove this method you can use the blade files.
-     * When using twill:module:make you can specify --bladeForm to use a blade form instead.
-     */
-    public function getForm(TwillModelContract $model): Form
-    {
-        $form = parent::getForm($model);
+  /**
+   * See the table builder docs for more information. If you remove this method you can use the blade files.
+   * When using twill:module:make you can specify --bladeForm to use a blade form instead.
+   */
+  public function getForm(TwillModelContract $model): Form
+  {
+    $form = parent::getForm($model);
 
-        $form->add(
-            Input::make()->name('description')->label('Description')
-        );
+    $form->add(
+      Input::make()
+        ->name('name')
+        ->label('Name'),
+    );
 
-        return $form;
-    }
+    $form->add(
+      Input::make()
+        ->name('email')
+        ->label('E-Mail'),
+    );
 
-    /**
-     * This is an example and can be removed if no modifications are needed to the table.
-     */
-    protected function additionalIndexTableColumns(): TableColumns
-    {
-        $table = parent::additionalIndexTableColumns();
+    return $form;
+  }
 
-        $table->add(
-            Text::make()->field('description')->title('Description')
-        );
+  /**
+   * This is an example and can be removed if no modifications are needed to the table.
+   */
+  protected function additionalIndexTableColumns(): TableColumns
+  {
+    $table = parent::additionalIndexTableColumns();
 
-        return $table;
-    }
+    $table->add(
+      Text::make()
+        ->field('name')
+        ->title('Name'),
+    );
+    $table->add(
+      Text::make()
+        ->field('email')
+        ->title('E-Mail'),
+    );
+
+    return $table;
+  }
 }
