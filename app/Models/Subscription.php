6fab9b08c0d49fca37226f566c9b7e747efb7412
Markus Sommer <markussom@gmail.com>
2023-11-05 16:08:35 +0100

    feat: remove slug

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-use A17\Twill\Models\Behaviors\HasSlug;
 use A17\Twill\Models\Model;
 
 class Subscription extends Model
 {
-  use HasSlug;
   protected $fillable = ['published', 'name', 'email', 'token', 'optin'];
 
   public $slugAttributes = ['email'];
--- app/Repositories/SubscriptionRepository.php
+++ app/Repositories/SubscriptionRepository.php
@@ -2,14 +2,11 @@
 
 namespace App\Repositories;
 
-use A17\Twill\Repositories\Behaviors\HandleSlugs;
 use A17\Twill\Repositories\ModuleRepository;
 use App\Models\Subscription;
 
 class SubscriptionRepository extends ModuleRepository
 {
-  use HandleSlugs;
-
   public function __construct(Subscription $model)
   {
     $this->model = $model;
