b5eff0ee944263b5af1d9f7aa7d61b92dfa74e2e
Markus Sommer <markussom@gmail.com>
2023-11-06 20:53:30 +0100

    feat: impreove styling of text only

--- resources/views/site/blocks/text.blade.php
+++ resources/views/site/blocks/text.blade.php
@@ -6,11 +6,11 @@
   <div
     class="{{ $hasImage ? 'lg:grid lg:grid-cols-2' : '' }} mx-auto max-w-screen-xl items-center gap-16 px-4 py-8 lg:px-6 lg:py-16"
   >
-    <div class="font-light text-stone-500 dark:text-stone-400 sm:text-lg">
+    <div class="{{ $hasImage ? '' : 'mx-auto' }} max-w-4xl font-light text-stone-500 dark:text-stone-400 sm:text-lg">
       <h2 class="mb-4 text-4xl font-extrabold tracking-tight text-stone-900 dark:text-white">
         {!! $block->input('title') !!}
       </h2>
-      <div class="format lg:format-lg dark:format-invert max-w-3xl">
+      <div class="format lg:format-lg dark:format-invert max-w-none">
         {!! $block->input('text') !!}
       </div>
     </div>
