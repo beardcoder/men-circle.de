9dddf9025a30731ceceb173d226b56e451496b53
Markus Sommer <markussom@gmail.com>
2023-11-06 08:46:04 +0100

    fix: correct spelling

--- database/migrations/2023_11_05_113432_create_subscriptions_tables copy.php
+++ database/migrations/2023_11_05_113432_create_subscriptions_tables copy.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSubscriptionsTables extends Migration
+class MyCreateSubscriptionsTables extends Migration
 {
   public function up()
   {
