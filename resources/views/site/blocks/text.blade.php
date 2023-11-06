b12864e774f87044d790719b506e975a418a6410
Markus Sommer <markussom@gmail.com>
2023-11-06 20:49:27 +0100

    fix: correct styling

--- resources/views/site/blocks/text.blade.php
+++ resources/views/site/blocks/text.blade.php
@@ -4,7 +4,7 @@
 
 <section class="bg-white dark:bg-stone-900">
   <div
-    class="{{ $hasImage ?? 'lg:grid lg:grid-cols-2' }} mx-auto max-w-screen-xl items-center gap-16 px-4 py-8 lg:px-6 lg:py-16"
+    class="{{ $hasImage ? 'lg:grid lg:grid-cols-2' : '' }} mx-auto max-w-screen-xl items-center gap-16 px-4 py-8 lg:px-6 lg:py-16"
   >
     <div class="font-light text-stone-500 dark:text-stone-400 sm:text-lg">
       <h2 class="mb-4 text-4xl font-extrabold tracking-tight text-stone-900 dark:text-white">
