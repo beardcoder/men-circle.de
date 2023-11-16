4b1e165f3dcc0102547c54c2e9a15105d9e0abd2
Markus Sommer <markussom@gmail.com>
2023-11-16 07:46:59 +0100

    feat: improve page edit

--- config/twill.php
+++ config/twill.php
@@ -4,6 +4,64 @@ return [
   'block_editor' => [
     'use_twill_blocks' => [],
     'crops' => [
+      'mobile' => [
+        [
+          'name' => 'mobile',
+          'ratio' => 1,
+        ],
+      ],
+      'flexible' => [
+        [
+          'name' => 'free',
+          'ratio' => 0,
+        ],
+        [
+          'name' => 'landscape',
+          'ratio' => 16 / 9,
+        ],
+        [
+          'name' => 'portrait',
+          'ratio' => 3 / 5,
+        ],
+      ],
+    ],
+    'hero' => [
+      'default' => [
+        [
+          'name' => 'default',
+          'ratio' => 16 / 9,
+        ],
+      ],
+      'mobile' => [
+        [
+          'name' => 'mobile',
+          'ratio' => 1,
+        ],
+      ],
+    ],
+    'tool_image' => [
+      'default' => [
+        [
+          'name' => 'default',
+          'ratio' => 1,
+        ],
+      ],
+    ],
+    'text_image' => [
+      'default' => [
+        [
+          'name' => 'default',
+          'ratio' => 0,
+        ],
+      ],
+    ],
+    'logo' => [
+      'default' => [
+        [
+          'name' => 'default',
+          'ratio' => 0,
+        ],
+      ],
       'logo' => [
         'default' => [
           [
--- resources/views/site/layouts/block.blade.php
+++ resources/views/site/layouts/block.blade.php
@@ -3,12 +3,14 @@
 
 <head>
   <title>#madewithtwill website</title>
+  @vite('resources/css/app.css')
 </head>
 
 <body>
   <div>
     @yield('content')
   </div>
+  @vite(['resources/js/app.js'])
 </body>
 
 </html>
