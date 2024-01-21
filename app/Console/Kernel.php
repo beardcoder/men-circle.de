0b04ef28792473141a9ff106ac1091bc8227ad3f
Markus Sommer <markussom@gmail.com>
2024-01-21 21:31:43 +0100

    feat: add sitemap

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
   protected function schedule(Schedule $schedule): void
   {
     $schedule->command('twill:lqip')->hourly();
+    $schedule->command('sitemap:generate')->hourly();
   }
 
   /**
