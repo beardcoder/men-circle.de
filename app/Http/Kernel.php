c8cf4b6124e8029af26f34cb6171390c376e1539
Markus Sommer <markussom@gmail.com>
2024-02-09 21:35:39 +0100

    feat: remove response cache

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,8 +65,5 @@ class Kernel extends HttpKernel
     'signed' => \App\Http\Middleware\ValidateSignature::class,
     'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-    'doNotCacheResponse' => \Spatie\ResponseCache\Middlewares\DoNotCacheResponse::class,
-    'cacheResponse' => \Spatie\ResponseCache\Middlewares\CacheResponse::class,
-    'check.flash.caching' => \App\Http\Middleware\CheckFlashMessageForCaching::class,
   ];
 }
--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,11 @@ use App\Http\Controllers\Frontend\NewsletterController;
 use App\Http\Controllers\Frontend\PageDisplayController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['check.flash.caching'])->group(function () {
-  Route::get('/', [PageDisplayController::class, 'home'])->name('frontend.home');
-  Route::get('{slug}', [PageDisplayController::class, 'show'])->name('frontend.page');
-  Route::get('events/next', [EventController::class, 'next'])->name('events.next');
-  Route::get('events/{event}', [EventController::class, 'show'])->name('events.show');
-  Route::get('event/{event}', [EventController::class, 'show'])->name('event.show');
-});
+Route::get('/', [PageDisplayController::class, 'home'])->name('frontend.home');
+Route::get('{slug}', [PageDisplayController::class, 'show'])->name('frontend.page');
+Route::get('events/next', [EventController::class, 'next'])->name('events.next');
+Route::get('events/{event}', [EventController::class, 'show'])->name('events.show');
+Route::get('event/{event}', [EventController::class, 'show'])->name('event.show');
 
 Route::get('events/{event}/ical', [EventController::class, 'ical'])->name('event.ical');
 Route::post('newsletter/register', [NewsletterController::class, 'register'])->name('newsletter.register');
