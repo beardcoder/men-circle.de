f7e7ca08b6dd61c1c277ffd209dee3aed339861f
Markus Sommer <markussom@gmail.com>
2024-03-17 09:18:03 +0100

    chore: update deps

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,16 +1,11 @@
 import '@fontsource-variable/jost'
 import 'leaflet/dist/leaflet.css'
 
-import { TwillImage } from '../../vendor/area17/twill-image'
-
-
 import('flowbite').then(({ initFlowbite }) => {
   initFlowbite()
 })
 
 document.addEventListener('DOMContentLoaded', () => {
-  new TwillImage()
-
   const map = document.getElementById('map')
   const leafletLoaded = new Event('leafletLoaded')
   if (map) {
