a29726a8002a43fb487536d1bc388ec5b5898dfa
Markus Sommer <markussom@gmail.com>
2024-03-25 20:25:38 +0100

    feat: add images

--- resources/views/mail/event/register.blade.php
+++ resources/views/mail/event/register.blade.php
@@ -1,82 +1,308 @@
-<!DOCTYPE html>
-<html lang="de">
+<!DOCTYPE
+  HTML
+  PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"
+>
+<html
+  xmlns="http://www.w3.org/1999/xhtml"
+  xmlns:v="urn:schemas-microsoft-com:vml"
+  xmlns:o="urn:schemas-microsoft-com:office:office"
+>
 
 <head>
-  <meta charset="UTF-8">
-  <title>Willkommen im Männerkreis</title>
-  <style>
+  <!--[if gte mso 9]>
+<xml>
+  <o:OfficeDocumentSettings>
+    <o:AllowPNG/>
+    <o:PixelsPerInch>96</o:PixelsPerInch>
+  </o:OfficeDocumentSettings>
+</xml>
+<![endif]-->
+  <meta
+    http-equiv="Content-Type"
+    content="text/html; charset=UTF-8"
+  >
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0"
+  >
+  <meta name="x-apple-disable-message-reformatting">
+  <!--[if !mso]><!-->
+  <meta
+    http-equiv="X-UA-Compatible"
+    content="IE=edge"
+  ><!--<![endif]-->
+  <title></title>
+
+  <style type="text/css">
+    @media only screen and (min-width: 720px) {
+      .u-row {
+        width: 700px !important;
+      }
+
+      .u-row .u-col {
+        vertical-align: top;
+      }
+
+      .u-row .u-col-100 {
+        width: 700px !important;
+      }
+    }
+
+    @media (max-width: 720px) {
+      .u-row-container {
+        max-width: 100% !important;
+        padding-left: 0px !important;
+        padding-right: 0px !important;
+      }
+
+      .u-row .u-col {
+        min-width: 320px !important;
+        max-width: 100% !important;
+        display: block !important;
+      }
+
+      .u-row {
+        width: 100% !important;
+      }
+
+      .u-col {
+        width: 100% !important;
+      }
+
+      .u-col>div {
+        margin: 0 auto;
+      }
+    }
+
     body {
-      font-family: Arial, sans-serif;
-      color: #333;
-      background-color: #f4f4f4;
-      line-height: 1.6;
+      margin: 0;
+      padding: 0;
     }
 
-    .container {
-      max-width: 600px;
-      margin: 20px auto;
-      padding: 20px;
-      background: #fff;
-      border: 1px solid #ddd;
-      border-radius: 5px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    table,
+    tr,
+    td {
+      vertical-align: top;
+      border-collapse: collapse;
     }
 
-    .header {
-      color: #444;
-      background-color: #B76F2B;
-      padding: 10px 0;
-      text-align: center;
+    p {
+      margin: 0;
     }
 
-    .content {
-      margin-top: 20px;
+    .ie-container table,
+    .mso-container table {
+      table-layout: fixed;
     }
 
-    .footer {
-      text-align: center;
-      margin-top: 40px;
-      font-size: 0.8em;
-      color: #666;
+    * {
+      line-height: inherit;
     }
 
-    .button {
-      display: inline-block;
-      background-color: #B76F2B;
-      color: white;
-      padding: 10px 20px;
-      text-decoration: none;
-      border-radius: 5px;
-      margin-top: 20px;
+    a[x-apple-data-detectors='true'] {
+      color: inherit !important;
+      text-decoration: none !important;
+    }
+
+    table,
+    td {
+      color: #000000;
+    }
+
+    #u_body a {
+      color: #0071e3;
+      text-decoration: underline;
+    }
+
+    @media (max-width: 480px) {
+      #u_content_text_4 .v-font-size {
+        font-size: 18px !important;
+      }
+
+      #u_content_text_4 .v-line-height {
+        line-height: 140% !important;
+      }
+
+      #u_content_text_7 .v-font-size {
+        font-size: 18px !important;
+      }
     }
   </style>
+
 </head>
 
-<body>
-  <div class="container">
-    <div class="header">
-      <h1>Willkommen im Männerkreis!</h1>
-    </div>
-    <div class="content">
-      <p>Hallo {{ $name }},</p>
-      <p>herzlich willkommen im Männerkreis! Wir freuen uns, dich in unserer Gemeinschaft begrüßen zu dürfen.</p>
-      <p>Hier findest du Gleichgesinnte, mit denen du dich über Themen wie Persönlichkeitsentwicklung, Yoga, Meditation
-        und vieles mehr austauschen kannst.</p>
-      <p>Deine Registrierung ist erfolgreich abgeschlossen. Du kannst nun alle Funktionen unserer Plattform nutzen.</p>
+<body
+  class="clean-body u_body"
+  style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #e7e7e7;color: #000000"
+>
+  <!--[if IE]><div class="ie-container"><![endif]-->
+  <!--[if mso]><div class="mso-container"><![endif]-->
+  <table
+    id="u_body"
+    style="border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;min-width: 320px;Margin: 0 auto;background-color: #e7e7e7;width:100%"
+    cellpadding="0"
+    cellspacing="0"
+  >
+    <tbody>
+      <tr style="vertical-align: top">
+        <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
+          <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td align="center" style="background-color: #e7e7e7;"><![endif]-->
 
-      <p>Wenn du Fragen hast oder Unterstützung benötigst, zögere nicht, uns zu kontaktieren.</p>
+          <div
+            class="u-row-container"
+            style="padding: 15px 15px 70px;background-color: transparent"
+          >
+            <div
+              class="u-row"
+              style="margin: 0 auto;min-width: 320px;max-width: 700px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;"
+            >
+              <div
+                style="border-collapse: collapse;display: table;width: 100%;height: 100%;background-color: transparent;"
+              >
+                <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 15px 15px 70px;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:700px;"><tr style="background-color: transparent;"><![endif]-->
 
-      <a
-        class="button"
-        href="{{ $loginUrl }}"
-      >Zum Login</a>
+                <!--[if (mso)|(IE)]><td align="center" width="700" style="width: 700px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;" valign="top"><![endif]-->
+                <div
+                  class="u-col u-col-100"
+                  style="max-width: 320px;min-width: 700px;display: table-cell;vertical-align: top;"
+                >
+                  <div
+                    style="height: 100%;width: 100% !important;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;"
+                  >
+                    <!--[if (!mso)&(!IE)]><!-->
+                    <div
+                      style="box-sizing: border-box; height: 100%; padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;"
+                    ><!--<![endif]-->
 
-      <p>Viele Grüße,<br>Dein Team vom Männerkreis</p>
-    </div>
-    <div class="footer">
-      <p>Diese E-Mail wurde automatisch erstellt. Bitte antworte nicht darauf.</p>
-    </div>
-  </div>
+                      <table
+                        style="font-family:helvetica,sans-serif;"
+                        role="presentation"
+                        cellpadding="0"
+                        cellspacing="0"
+                        width="100%"
+                        border="0"
+                      >
+                        <tbody>
+                          <tr>
+                            <td
+                              style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:helvetica,sans-serif;"
+                              align="left"
+                            >
+
+                              <table
+                                width="100%"
+                                cellpadding="0"
+                                cellspacing="0"
+                                border="0"
+                              >
+                                <tr>
+                                  <td
+                                    style="padding-right: 0px;padding-left: 0px;"
+                                    align="center"
+                                  >
+                                    <img
+                                      align="center"
+                                      border="0"
+                                      src="https://assets.unlayer.com/projects/0/1711394182140-email.jpg"
+                                      alt=""
+                                      title=""
+                                      style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 100%;max-width: 680px;"
+                                      width="680"
+                                    />
+                                  </td>
+                                </tr>
+                              </table>
+
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+
+                      <table
+                        id="u_content_text_4"
+                        style="font-family:helvetica,sans-serif;"
+                        role="presentation"
+                        cellpadding="0"
+                        cellspacing="0"
+                        width="100%"
+                        border="0"
+                      >
+                        <tbody>
+                          <tr>
+                            <td
+                              style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:helvetica,sans-serif;"
+                              align="left"
+                            >
+
+                              <div
+                                class="v-line-height v-font-size"
+                                style="font-size: 21px; font-weight: 700; line-height: 140%; text-align: left; word-wrap: break-word;"
+                              >
+                                <div>
+                                  <div>Willkommen im Männerkreis!</div>
+                                </div>
+                              </div>
+
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+
+                      <table
+                        id="u_content_text_7"
+                        style="font-family:helvetica,sans-serif;"
+                        role="presentation"
+                        cellpadding="0"
+                        cellspacing="0"
+                        width="100%"
+                        border="0"
+                      >
+                        <tbody>
+                          <tr>
+                            <td
+                              style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:helvetica,sans-serif;"
+                              align="left"
+                            >
+
+                              <div
+                                class="v-line-height v-font-size"
+                                style="font-size: 14px; font-weight: 700; line-height: 140%; text-align: left; word-wrap: break-word;"
+                              >
+                                <p style="line-height: 140%;">Willkommen {{ $name }},</p>
+                                <div>
+                                  <div>Ich freuen mich, dich in unserer Gemeinschaft begrüßen zu dürfen.<br /><br />Du
+                                    hast dich für das Event am {{ $date }} angemeldet.</div>
+                                  <div> </div>
+                                  <div>Um deine Wartezeit zu verkürzen kannst du dich unserer Telegram Gruppe
+                                    anschließen: <a
+                                      href="https://t.me/+J8e6gcycMLs2YTIy">https://t.me/+J8e6gcycMLs2YTIy</a><br /><br />
+                                  </div>
+                                  <div>Bis bald.</div>
+                                </div>
+                              </div>
+
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+
+                      <!--[if (!mso)&(!IE)]><!-->
+                    </div><!--<![endif]-->
+                  </div>
+                </div>
+                <!--[if (mso)|(IE)]></td><![endif]-->
+                <!--[if (mso)|(IE)]></tr></table></td></tr></table><![endif]-->
+              </div>
+            </div>
+          </div>
+
+          <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <!--[if mso]></div><![endif]-->
+  <!--[if IE]></div><![endif]-->
 </body>
 
 </html>
