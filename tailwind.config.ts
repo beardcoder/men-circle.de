e3d98b8cd480ad1ad323523bd9567f899651aec6
Markus Sommer <markus.sommer@fullhaus.de>
2024-08-02 21:30:54 +0200

    feat: remove tailwind
    Signed-off-by: Markus Sommer <markus.sommer@fullhaus.de>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,6 @@ export default {
         './packages/sitepackage/**/*.ts',
         './packages/sitepackage/**/*.js',
         './packages/sitepackage/**/*.yaml',
-        './node_modules/flowbite/**/*.js',
     ],
     darkMode: 'media',
     theme: {
