710e62c7b8f9abcfd37bbc9ee6d30cabf7938a6b
Markus Sommer <markus@letsbenow.de>
2024-11-30 21:14:50 +0100

    fix: add root icon

--- packages/sitepackage/Configuration/JavaScriptModules.php
+++ packages/sitepackage/Configuration/JavaScriptModules.php
@@ -7,9 +7,6 @@ return [
     'imports' => [
         '@mens-circle/sitepackage/' => [
             'path' => 'EXT:sitepackage/Resources/Public/Backend/Scripts/',
-            'exclude' => [
-                'EXT:EXT:sitepackage/Resources/Public/Backend/Scripts/Contrib/',
-            ],
         ],
     ],
 ];
